6700b0fd25bc7dea78563711e6045aaddabeb63c
Rutuza <dalal.jyotika05@gmail.com>
2024-02-19 23:43:21 +0530

    navbar initialised

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Darkmode />
+      <Navbar></Navbar>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-// import { Navbutton } from "../img/icon";
+import logo from "../assets/svg/logo.svg";
 
 
 function Navbar() {
   return (
-    <header id="header">
-      <div className="container">
-        <div className="logo">
-          <img src={logo} alt="" srcset="" />
-        </div>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/skills">Skills</a></li>
-            <li><a href="/work">Work</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </nav>
-
+    <nav>
+      <div>
+        <img src={logo} alt="" srcset="" />
       </div>
-    </header>
+      <div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
