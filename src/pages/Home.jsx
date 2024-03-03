46267a1ae5509758fe525b2a38082e00d8ef8248
Rutuza <dalal.jyotika05@gmail.com>
2024-03-03 16:24:12 +0530

    mobile view home page made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import circles from '../assets/svg/circles.svg';
+import ellipse from '../assets/svg/ellipse.svg';
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function Home() {
@@ -13,13 +13,13 @@ function Home() {
     <div className='m-8 font-pop max-sm:mt-28'>
       <div className="max-sm:grid max-sm:gap-y-12 flex justify-between items-center md:text-3xl">
         <div className="text-content">
-          <h1 className='font-bold text-6xl max-sm:text-2xl'>Hi <br />
+          <h1 className='font-bold text-6xl max-sm:text-3xl'>Hi <br />
           I am <span>{text}</span><Cursor cursorColor='#936BE8' /></h1>
 
-          <h6 className='mt-5 max-sm:mt-2 max-sm:text-sm'>Design and code what you think</h6>
+          <h6 className='text-lg mt-3'>Design and code what you think</h6>
         </div>
         <div className="circles">
-          <img src={circles} alt="" />
+          <img src={ellipse} alt=""/>
         </div>
       </div>
     </div>
