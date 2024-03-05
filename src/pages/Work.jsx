de8a9696c42d4ab31a5907703d25df92413c7074
Rutuza <dalal.jyotika05@gmail.com>
2024-03-05 23:54:39 +0530

    something have been done

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -5,14 +5,21 @@ function Work() {
     <>
       <section className='font-pop'>
         <center className="text-4xl font-bold italic m-5">Some Inspiration</center>
-        <div className='p-10'> {/*list */}
-            <ul className='flex gap-32'>
+        <div className='p-8'> {/*list */}
+            <ul className='flex gap-32 p-10'>
                 <li className='text-xl font-semibold hover:text-[#ff4773]'><a href="#">All</a></li>
                 <li className='text-xl font-semibold hover:text-[#ff4773]'><a href="#">Projects</a></li>
                 <li className='text-xl font-semibold hover:text-[#ff4773]'><a href="#">Designs</a></li>
                 <li className='text-xl font-semibold hover:text-[#ff4773]'><a href="#">Photos</a></li>
                 <li className='text-xl font-semibold hover:text-[#ff4773]'><a href="#">Quotes</a></li>
             </ul>
+
+          <div className=" flex flex-wrap">
+            <div className="bg-gray-200 w-5/12 h-80"></div>
+            <div className='bg-green-200 w-1/2 h-80'></div>
+            
+          </div>
+
         </div>
       </section>
     </>
