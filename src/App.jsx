dfb3d384688f7510a2d40c14c94a78d21e0d0e51
Rutuza <dalal.jyotika05@gmail.com>
2024-03-09 16:06:17 +0530

    map function passed in work page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
-
+  
   return (
     <>
       <div id='root'>
