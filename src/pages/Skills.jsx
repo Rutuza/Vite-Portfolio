557c3e26fa674b42952957ee1b8b3643a574e6eb
Rutuza <dalal.jyotika05@gmail.com>
2024-03-15 01:02:51 +0530

    to look after certicates

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,11 +6,15 @@ import atom from "../assets/svg/moleculewithcircle.svg";
 import dna from "../assets/svg/dnawithcircle.svg";
 import code_icon from "../assets/svg/codewithcircle.svg";
 
+import mongoDb from "../assets/svg/mongoDb_icon.svg";
+import arrow from "../assets/svg/right_arrow.svg";
+
 function Skills() {
   return (
     <div id="skills">
       <center className="text-4xl font-bold italic m-5">Skills</center>
       <div className="p-10">
+
         <section className="relative font-pop max-sm:w-full lg:flex lg:items-center">
           <div className="container xl:w-9/12">
             <h1 className="font-bold max-sm:text-xl sm:text-2xl lg:text-2xl xl:text-3xl">
@@ -40,7 +44,7 @@ function Skills() {
         </section>
 
         {/* techstack */}
-        <section className="techstacks mt-20">
+        <section className="techstacks mt-36">
           <div className="flex flex-wrap justify-center items-center text-center gap-40">
             <div className="skill-box">
               <div className="skill-title flex flex-col items-center p-0.5 mb-0.5 relative">
@@ -77,37 +81,12 @@ function Skills() {
         </section>
 
         {/* certifications */}
-        {/* <section className="certificates-section">
-          <div className="flex justify-between items-center flex-wrap">
-            <div className="w-9/12">
-              <h1 className="font-bold text-3xl">Professional Development</h1>
-              <p className="mt-4 text-justify w-1/2">
-                I have pursued various certifications throughout my career to
-                continuously enhance my skills and stay updated with the latest
-                industry standards. Among my certifications, I hold a **MongoDB
-                for SQL Experts** certification from MongoDb University, earned
-                in September 2023, which signifies my proficiency in leading and
-                managing projects effectively. Additionally, I completed a **CSS
-                Skills HackerRank Test** on HackerRank in 2022, providing me
-                with comprehensive knowledge in my CSS progressive skills which
-                I am learning. These certifications reflect my commitment to
-                professional development and my ability to tackle diverse
-                challenges in the fields of project management, data science,
-                and cloud computing.
-              </p>
-            </div>
 
-            <div>
-              <img src={sparkle} alt="" />
-            </div>
-          </div>
-        </section> */}
+        <section className="relative font-pop max-sm:w-full mt-24">
 
-        <section className="relative font-pop max-sm:w-full mt-20">
-
-          <div className="lg:flex lg:items-center">
+          <div className="lg:flex lg:items-center bg-red-200">
             <div className="container xl:w-9/12">
-              <h2 className="font-bold max-sm:text-xl sm:text-2xl lg:text-2xl xl:text-3xl">Professional achievements</h2>
+              <h2 className="font-bold max-sm:text-xl sm:text-2xl lg:text-2xl xl:text-3xl relative">Professional achievements</h2>
               <p className="text-justify break-words mt-4 lg:w-9/12">
                   I have pursued various certifications throughout my career to
                   continuously enhance my skills and stay updated with the latest
@@ -133,6 +112,31 @@ function Skills() {
             </div>
           </div>
         </section>
+
+          <div className="container mt-8">
+            <div className="card1">
+              <div className="mongodb w-1/2 h-56 border-2 rounded-lg flex flex-wrap justify-center items-center bg-black">
+                    <img src={mongoDb} alt="" />
+              </div>
+              <div className="w-full mt-4">
+                <a href="#" className="flex justify-evenly items-center">
+                  <h3 className="max-sm:text-sm max-sm:font-medium">MongoDB for SQL Experts</h3>
+                  <img src={arrow} alt="" />
+                </a>
+              </div>
+            </div>
+            <div className="card2 py-8">
+              <div className="mongodb max-sm:w-1/2 h-56 border-2 rounded-lg flex justify-center items-center bg-black">
+                    <img src={mongoDb} alt="" />
+              </div>
+              <div className="w-full mt-4">
+                <a href="#" className="flex justify-evenly items-center">
+                  <h3 className="max-sm:text-sm max-sm:font-medium">HackerRank CSS Skills Test</h3>
+                  <img src={arrow} alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
