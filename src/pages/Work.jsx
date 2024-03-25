923a6ca1ff207481443b073706b384eeca158e0f
Rutuza <dalal.jyotika05@gmail.com>
2024-03-25 20:34:29 +0530

    work page initiated

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 function Work() {
-  let work_des = ['All', 'Projects', 'Designs', 'Photos', 'Quotes'];
-  let img_set = ['weather app', 'portfolio', 'airbnb-img',];
-
   return (
     <>
-      <section className='font-pop'>
-        <center className="text-4xl font-bold italic m-5">Work</center>
-      </section>
+     <div className='p-2 font-pop bg-cream'>
+        <center>Work</center>
+        <div>
+            <div>
+                <h1 className='max-sm:text-2xl font-semibold'>The power of design thinking</h1>
+                <p className='text-justify mt-4'>The strength of design thinking is found in its capacity to foster creativity, human-centered solutions, and efficient problem-solving in a variety of contexts. It's a way of thinking that prioritizes collaboration, empathy, and experimentation, which makes it a useful strategy in the complicated and quickly changing world of today.</p>
+            </div>
+            <div></div>
+        </div>
+        <div className="cards flex flex-wrap flex-row mt-4 gap-4">
+            <div className='bg-gray-400 w-40 h-96 border-2 rounded-md'></div>
+            <div className='bg-gray-400 w-40 h-96 border-2 rounded-md'></div>
+            <div className='bg-gray-400 w-40 h-96 border-2 rounded-md'></div>
+            <div className='bg-gray-400 w-40 h-96 border-2 rounded-md'></div>
+        </div>
+     </div> 
     </>
   );
 }
