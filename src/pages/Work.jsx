9054d746a1629ce1788c3be971bdcbdb3db7ae73
Rutuza <dalal.jyotika05@gmail.com>
2024-03-31 14:09:35 +0530

    weather desc added

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -40,12 +40,15 @@ function Work() {
 
           <hr className='border-black mt-8'/>
 
-          <div className="projects-container mt-8">
-            <div className="weather-app">
+          <div className="projects-container">
+            <div className="weather-app p-8">
               <div>
                 <h1 className='text-3xl font-pop font-semibold'>Weather Forecasting App</h1>
                 <p className='w-1/2 mt-4 text-justify'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Our weather forecasting application provides users with up-to-date weather information in a visually appealing and user-friendly interface. Utilizing HTML for structure, CSS and Sass for styling, and JavaScript for interactivity, our application delivers accurate weather forecasts based on location data. Users can input their location or allow the application to access their current location automatically. The application dynamically updates weather data, displaying current conditions, hourly forecasts, and multi-day outlooks. With intuitive design and real-time updates, our weather app ensures users are prepared for whatever the forecast brings.
+                <br />
+                <br />
+                In summary, our weather forecasting application offers a seamless blend of HTML, CSS, Sass, and JavaScript to deliver a feature-rich and visually appealing user experience. Whether users are planning their day or preparing for a trip, our application provides the essential weather information they need to stay informed and make informed decisions
                 </p>
               </div>
               <div className="anonymous-img"></div>
@@ -62,11 +65,13 @@ function Work() {
               </div>
             </section>
 
-            <div className="airbnb-clone mt-8">
-              <div className='w-1/2'>
-                <h1 className='text-3xl'>Airbnb clone</h1>
-                <p className='text-justify mt-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div className="airbnb-clone">
+              <div className='p-8'>
+                <div className='w-1/2'>
+                  <h1 className='text-3xl'>Airbnb clone</h1>
+                  <p className='text-justify mt-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
+                </div>
               </div>
             </div>
 
