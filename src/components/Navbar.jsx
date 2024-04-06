315bf3e85b19c4c274881c856c89f0ca4864b8ea
Rutuza <dalal.jyotika05@gmail.com>
2024-04-06 23:50:01 +0530

    anonymous commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div className='sticky top-0 bg-white z-10'>
       <header className='relative'>
-        <nav className='py-2 px-8 flex justify-between items-center border-b-2 border-gray-900'>
+        <nav className='px-10 flex justify-between items-center border-b-2 border-gray-900'>
 
           <div className='flex justify-center items-start gap-2'>
             <img src={logo} alt="" />
@@ -18,11 +18,11 @@ function Navbar() {
 
             <div className={`max-sm:absolute top-0 -left-0 py-5 w-full max-sm:h-48 duration-700 ease-in-out max-sm:text-white max-sm:bg-warmGray flex justify-around sm:justify-end sm:translate-y-0 ${isVisible && ' -translate-y-[300%]'} `}>
               <ul className='flex items-center sm:justify-end sm:gap-8 gap-4 font-semibold max-sm:flex-wrap max-sm:gap-x-0 w-4/5 font-pop'>
-                <li className='max-sm:w-1/3'><a href="#">Home</a></li>
-                <li className='max-sm:w-1/3'><a href="#">About</a></li>
-                <li className='max-sm:w-1/3'><a href="#">Skills</a></li>
-                <li className='max-sm:w-1/3'><a href="#">Work</a></li>
-                <li className='max-sm:w-1/3'><a href="#">Contact</a></li>
+                <li className='max-sm:w-1/3 hover:border-b-2 hover:border-violet-400'><a href="#">Home</a></li>
+                <li className='max-sm:w-1/3 hover:border-b-2 hover:border-violet-400'><a href="#">About</a></li>
+                <li className='max-sm:w-1/3 hover:border-b-2 hover:border-violet-400'><a href="#">Skills</a></li>
+                <li className='max-sm:w-1/3 hover:border-b-2 hover:border-violet-400'><a href="#">Work</a></li>
+                <li className='max-sm:w-1/3 hover:border-b-2 hover:border-violet-400'><a href="#">Contact</a></li>
               </ul>
               <div className='sm:hidden'><RxCross2 className='w-5 h-5' onClick={() => {setIsVisible(!isVisible)}}/></div>
 
