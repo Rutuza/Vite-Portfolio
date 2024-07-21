a584a0630a24cd637014fea6c80bf9069d3ddd22
Rutuza <dalal.jyotika05@gmail.com>
2024-07-21 18:05:25 +0530

    single application done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
+import React from 'react'
 import './App.css'
+
 import './components/Navbar'
-
-
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
 import About from './pages/About'
 import Skills from './pages/Skills'
 import Work from './pages/Work'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
 
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
 
 function App() {
   
   return (
     <>
-      <div id='root'>
+    <div className="App">
+      <Navbar />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+
+
+
+
+
+
+
+
+
+      {/* <div id='root'>
         <Router>
           <Navbar />
             <Routes>
-              <Route path='/' element={<Home/>} />
               <Route path='/about' element={<About/>} />
               <Route path='/skills' element={<Skills/>} />
               <Route path='/work' element={<Work/>} />
@@ -30,8 +42,8 @@ function App() {
             </Routes>
 
         </Router>
-          {/* <Footer /> */}
-      </div>
+          
+      </div> */}
       
       
     </>
