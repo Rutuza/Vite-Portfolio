f0ac9b70b0c4eb4dfb31246962197c47f941119f
Rutuza <dalal.jyotika05@gmail.com>
2024-08-25 00:36:57 +0530

    work page commit

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import cameraLens from '../assets/images/camera lens.jpg';
+import ezybit from '../assets/images/Ezybit.png';
 
 function Work() {
   return (
@@ -7,12 +9,18 @@ function Work() {
       <div>
       <center className="text-4xl font-bold italic m-4">Work</center>
       <div className='mt-10'>
-        <ul className='flex flex-row gap-20 items-center justify-center font-semibold'>
-          <li><a href="#">All</a></li>
-          <li><a href="#">Projects</a></li>
-          <li><a href="#">Logo</a></li>
-          <li><a href="#">Gallery</a></li>
+        <ul className='flex flex-row gap-32 max-sm:gap-10 items-center justify-center font-medium'>
+          <li><a href="#" className='hover:text-violet-500'>All</a></li>
+          <li><a href="#" className='hover:text-violet-500'>Projects</a></li>
+          <li><a href="#" className='hover:text-violet-500'>Logo</a></li>
+          <li><a href="#" className='hover:text-violet-500'>Gallery</a></li>
         </ul>
+      </div>
+
+      <div className="images max-sm:mt-8 max-sm:p-1">
+        <img src={cameraLens} alt="" />
+        <img src={ezybit} alt="" />
+        <img src="" alt="" />
       </div>
       </div>
     </>
