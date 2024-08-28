c3ae39503059eabab88cd51326f774430a543262
Rutuza <dalal.jyotika05@gmail.com>
2024-08-28 23:58:45 +0530

    anonymous commit

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,15 +5,14 @@ import chatappImg from '../assets/images/chatapp.jpg';
 import ezybit from '../assets/images/SDE app.jpg';
 import umbrella from '../assets/images/umbrella.jpg';
 
-
-
 function Projects() {
   return (
     <>
-     <div className='p-8'>
-      <h3 className='font-pop font-bold text-2xl'>Projects</h3>
+     <div className='p-2 m-10'>
+      <div className='p-10'>
+        <h3 className='font-pop font-bold text-2xl'>Projects</h3>
 
-      <div className='mt-4 flex'>
+      <div className='mt-8 flex flex-row flex-wrap gap-14 items-center justify-center'>
         <div>
           <img src={chatappImg} alt="" className=''/>
           <p className='mt-3 font-pop font-bold text-xl'>Chat Application</p>
@@ -23,6 +22,7 @@ function Projects() {
           <img src={umbrella} alt="" className=''/>
           <p className='mt-3 font-pop font-bold text-xl'>Weather App</p>
         </div>
+      </div>
       </div>
      </div>
     </>
