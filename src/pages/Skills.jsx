35dfa51a3488a71907e6987bcc9a6bd58837bab5
Rutuza <dalal.jyotika05@gmail.com>
2024-09-17 15:14:26 +0530

    skills writen

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,40 +2,110 @@ import React from 'react';
 
 function Skills() {
   return (
-    <div className='flex flex-wrap gap-8'>
-      <div className="skill1 bg-slate-500 relative w-48 h-48 flex justify-center items-center flex-col">
+    <div className='flex flex-wrap gap-10 p-16 m-10 justify-center items-center'>
+      <div className="skill1 relative w-48 h-48 flex justify-center items-center flex-col">
         <h1 className='text-7xl'>01</h1>
         <div className='flex flex-col justify-center items-center'>
           <h4 className='text-sm font-semibold'>HTML</h4>
           <h6 className='text-sm italic'>Website structure definition</h6>
         </div>
       </div>
-      <div className="skill1 bg-slate-500 relative w-48 h-48 flex justify-center items-center flex-col">
-        <h1 className='text-7xl'>01</h1>
+      <div className="skill2 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>02</h1>
         <div className='flex flex-col justify-center items-center'>
-          <h4 className='text-sm font-semibold'>HTML</h4>
-          <h6 className='text-sm italic'>Website structure definition</h6>
+          <h4 className='text-sm font-semibold'>CSS</h4>
+          <h6 className='text-sm italic'>Styling & Layout of website</h6>
         </div>
       </div>
-      <div className="skill1 bg-slate-500 relative w-48 h-48 flex justify-center items-center flex-col">
-        <h1 className='text-7xl'>01</h1>
+      <div className="skill3 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>03</h1>
         <div className='flex flex-col justify-center items-center'>
-          <h4 className='text-sm font-semibold'>HTML</h4>
-          <h6 className='text-sm italic'>Website structure definition</h6>
+          <h4 className='text-sm font-semibold'>React JS</h4>
+          <h6 className='text-sm italic'>Dynamic UI Component</h6>
         </div>
       </div>
-      <div className="skill1 bg-slate-500 relative w-48 h-48 flex justify-center items-center flex-col">
-        <h1 className='text-7xl'>01</h1>
+      <div className="skill4 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>04</h1>
         <div className='flex flex-col justify-center items-center'>
-          <h4 className='text-sm font-semibold'>HTML</h4>
-          <h6 className='text-sm italic'>Website structure definition</h6>
+          <h4 className='text-sm font-semibold'>Node JS</h4>
+          <h6 className='text-sm italic'>Utility based CSS framework</h6>
         </div>
       </div>
-      <div className="skill1 bg-slate-500 relative w-48 h-48 flex justify-center items-center flex-col">
-        <h1 className='text-7xl'>01</h1>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>05</h1>
         <div className='flex flex-col justify-center items-center'>
-          <h4 className='text-sm font-semibold'>HTML</h4>
-          <h6 className='text-sm italic'>Website structure definition</h6>
+          <h4 className='text-sm font-semibold'>Tailwind CSS</h4>
+          <h6 className='text-sm italic'>Utility based CSS framework</h6>
+        </div>
+      </div>
+      <div className="skill6 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>06</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>SASS SCSS</h4>
+          <h6 className='text-sm italic'>Advanced CSS Preprocessor</h6>
+        </div>
+      </div>
+      <div className="skill7 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>07</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>UI/UX Design</h4>
+          <h6 className='text-sm italic'>User Experience Design</h6>
+        </div>
+      </div>
+      <div className="skill8 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>08</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>Bootstrap</h4>
+          <h6 className='text-sm italic'>Utility based CSS framework</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>09</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>Java</h4>
+          <h6 className='text-sm italic'>Cross-platform programming</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>10</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>C++</h4>
+          <h6 className='text-sm italic'>Utility based CSS framework</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>11</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>SQL Server</h4>
+          <h6 className='text-sm italic'>Relational database querying</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>12</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>Oracle pl/sql</h4>
+          <h6 className='text-sm italic'>Oracle procedural language</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>13</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>MongoDB</h4>
+          <h6 className='text-sm italic'>NoSQL database solution</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>14</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>AIML</h4>
+          <h6 className='text-sm italic'>Data-driven intelligent systems</h6>
+        </div>
+      </div>
+      <div className="skill5 relative w-48 h-48 flex justify-center items-center flex-col">
+        <h1 className='text-7xl'>15</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h4 className='text-sm font-semibold'>JavaScript</h4>
+          <h6 className='text-sm italic'>Data-driven intelligent systems</h6>
         </div>
       </div>
     </div>
