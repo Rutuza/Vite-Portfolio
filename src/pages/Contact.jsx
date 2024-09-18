296198cb0682b4d5f8bb8f332600f0b65f71e4f6
Rutuza <dalal.jyotika05@gmail.com>
2024-09-18 13:41:05 +0530

    contact page need to check

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Contact() {
   return (
     <>
-     <div className='px-16 p-8'>
+      <div className=" p-8">
         <div className="title text-xl font-pop font-semibold">Contact</div>
-        <h1 className='text-right text-6xl'>hello.jyotikadalal.com</h1>
+        <h1 className="text-right text-6xl">hello.jyotikadalal.com</h1>
 
-        <form action="">
-          <div className="flex flex-row gap-16 items-center">
-            <div className='grid w-80'>
-              <label htmlFor="">Name</label>
-              <input type="text" name="" id="" placeholder='name' className='border-black border'/>
+        <div className="flex justify-center items-center mt-16">
+          <form className="w-1/2 bg-red-300 relative">
+            <div className="flex justify-between items-center flex-row gap-14">
+              <div className="flex flex-col w-1/2">
+                <label htmlFor="">Name</label>
+                <input type="text" name="" id="name" placeholder="" className="border-b-2 border-black outline-none bg-transparent" required/>
+              </div>
+              <div className="flex flex-col w-1/2">
+                <label htmlFor="">Email</label>
+                <input type="email" name="" id="name" placeholder="" className="border-b-2 border-black outline-none bg-transparent" required/>
+              </div>
             </div>
-            <div className='grid w-80'>
-              <label htmlFor="">Email</label>
-              <input type="email" name="" id="" placeholder='name' className='border-black border'/>
+            <div>
+              <div className="flex flex-col my-4">
+                <label htmlFor="">Subject</label>
+                <input type="text" name="" id="" className="border-b-2 border-black outline-none bg-transparent" required/>
+              </div>
+              <div className="flex flex-col my-4">
+                <label htmlFor="">Message</label>
+                <textarea name="message" id="message" className="border-b-2 border-black outline-none bg-transparent" required></textarea>
+              </div>
             </div>
-
-          </div>
-            <div className="subject w-1/2 grid">
-              <label htmlFor="">Subject</label>
-              <input type="text" name="" id="" placeholder='subject' className='border-black border'/>
-            </div>
-
-            <div className="mesaage w-1/2 grid">
-              <label htmlFor="">Message</label>
-              <textarea name="" id=""  placeholder='message' className='border-black border'></textarea>
-            </div>
-        </form>
-     </div> 
+          </form>
+        </div>
+      </div>
     </>
   );
 }
